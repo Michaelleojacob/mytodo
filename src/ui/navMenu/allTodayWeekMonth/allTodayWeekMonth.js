8320dde372e380a6ed976bb8c56ce3daa424428b
Michael <michaelleojacob@gmail.com>
2021-09-25 21:58:21 -0700

    styling is going both well and horribly haha haha ha. This is terrible. I was getting the tasks based on text content, so I will have to rework logic due to some of my styling decisions. lol what fun. I also was exposed to a bug, where projects new item is not being rendered to the projects wrapper lol, imagine. So that will be up on the list next.

--- src/ui/navMenu/allTodayWeekMonth/allTodayWeekMonth.js
+++ src/ui/navMenu/allTodayWeekMonth/allTodayWeekMonth.js
@@ -1,3 +1,4 @@
+import '../navMenu.css';
 import domFactory from '../../../domFactory/domFactory';
 
 export default class AllTodayWeekMonth {
@@ -6,28 +7,60 @@ export default class AllTodayWeekMonth {
 		this.allTodayMonthWrap();
 		this.allTodayMonth();
 	}
+	// static allTodayMonthWrap() {
+	// 	return (this.allTodayMonthWrapper = domFactory.createDiv({
+	// 		elementName: 'allTodayMonthWrapper',
+	// 		parent: this.parent,
+	// 	}));
+	// }
+	// static items = [
+	// 	{ name: 'all', classes: ['sortByFrom'] },
+	// 	{ name: 'today', classes: ['sortByTime'] },
+	// 	{ name: 'week', classes: ['sortByTime'] },
+	// 	{ name: 'month', classes: ['sortByTime'] },
+	// ];
+	// static allTodayMonth() {
+	// 	AllTodayWeekMonth.items.forEach(({ name, classes }) => {
+	// 		const element = domFactory.elementWithMultipleClasses({
+	// 			elementName: name,
+	// 			type: 'button',
+	// 			classes: ['navButton', name, ...classes],
+	// 			text: name,
+	// 			parent: this.allTodayMonthWrapper,
+	// 		});
+	// 		return element;
+	// 	});
+	// }
 	static allTodayMonthWrap() {
-		return (this.allTodayMonthWrapper = domFactory.createDiv({
-			elementName: 'allTodayMonthWrapper',
-			parent: this.parent,
-		}));
+		this.allTodayMonthWrapper = domFactory.domElement({
+			classes: ['allTodayMonthWrapper'],
+		});
+		this.parent.appendChild(this.allTodayMonthWrapper);
 	}
 	static items = [
-		{ name: 'all', classes: ['sortByFrom'] },
-		{ name: 'today', classes: ['sortByTime'] },
-		{ name: 'week', classes: ['sortByTime'] },
-		{ name: 'month', classes: ['sortByTime'] },
+		{ name: 'all', icon: ['fa-list'] },
+		{ name: 'today', icon: ['fa-inbox'] },
+		{ name: 'week', icon: ['fa-calendar-week'] },
+		{ name: 'month', icon: ['fa-calendar-alt'] },
 	];
 	static allTodayMonth() {
-		AllTodayWeekMonth.items.forEach(({ name, classes }) => {
-			const element = domFactory.elementWithMultipleClasses({
-				elementName: name,
+		AllTodayWeekMonth.items.forEach(({ name, icon }) => {
+			const element = domFactory.domElement({
 				type: 'button',
-				classes: ['navButton', name, ...classes],
-				text: name,
-				parent: this.allTodayMonthWrapper,
+				classes: ['navButton', name],
+				children: [
+					//icon
+					domFactory.domElement({
+						classes: ['navIcon', 'fa', icon],
+					}),
+					//button
+					domFactory.domElement({
+						classes: ['navButtonText'],
+						text: name,
+					}),
+				],
 			});
-			return element;
+			this.allTodayMonthWrapper.appendChild(element);
 		});
 	}
 }
